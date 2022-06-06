3f9fb5b6b06b35506e94816bccdd6784f8912cde
Giovanne Berteli Comba <joebcomba@gmail.com>
2022-06-05 21:14:35 -0300

    teste

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Card from './componentes/card';
+
+const App = () => {
+  return (
+    <div>
+      <Card nome="Blue" idade="1"/>
+      <Card nome="Giovanne" idade="20"/>
+      <h1>oi!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/card/index.jsx
@@ -0,0 +1,8 @@
+const Card = (props) => {
+  return <div>
+    <h1>{props.nome}</h1>
+    <p>{props.idade}</p>
+  </div>
+}
+
+export default Card;
